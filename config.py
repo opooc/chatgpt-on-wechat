ac794ece91587a7b69dd6f265f58dd62219911b8
opooc <6850365@qq.com>
2023-05-08 02:02:21 +0800

    Update config.py opooc

--- config.py
+++ config.py
@@ -165,6 +165,8 @@ def load_config():
     if not os.path.exists(config_path):
         logger.info("配置文件不存在，将使用config-template.json模板")
         config_path = "./config-template.json"
+    else:
+        logger.info("opooc")
 
     config_str = read_file(config_path)
     logger.debug("[INIT] config str: {}".format(config_str))
